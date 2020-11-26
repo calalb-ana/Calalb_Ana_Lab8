407067b0722e6be498a6936c2368cc0d5b6dbcf1
Ana Calalb <calalb.ana@gmail.com>
2020-11-26 18:52:23 +0200

    Add project files.

---
+++ Data/Calalb_Ana_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Calalb_Ana_Lab8.Models;
+
+namespace Calalb_Ana_Lab8.Data
+{
+    public class Calalb_Ana_Lab8Context : DbContext
+    {
+        public Calalb_Ana_Lab8Context (DbContextOptions<Calalb_Ana_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Calalb_Ana_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Calalb_Ana_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Calalb_Ana_Lab8.Models.Category> Category { get; set; }
+    }
+}
